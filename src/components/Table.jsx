ea8578a63c95746ba1d8848b88b036d7a226ff8e
mario-website <mario.websie@gmail.com>
2022-10-09 15:33:17 +0200

    used better naming and exported most functions to functions.js

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,243 +3,39 @@ import {ACTION_TYPES, COURIER_NAMES, VARIABLES} from "../store/postActionTypes";
 import {INITIAL_STATE, postReducer} from "../store/postReducer";
 import normalizerNames from "../store/normalizerNames";
 import {dynamicSort} from "../store/functions";
+import {getData, formattingData, setNewData} from "./functions";
 
 const Table = () => {
   const {PARCEL_MONKEY, PARCEL2GO} = COURIER_NAMES;
   const {SLOW, MEDIUM, FAST} = VARIABLES;
   const [data, setData] = useState([]);
   const [state, dispatch] = useReducer(postReducer, INITIAL_STATE);
-  const [allResponses, setAllResponses] = useState([
-    {SLOW: []},
-    {MEDIUM: []},
-    {FAST: []},
-  ]);
+  const [allResponses, setAllResponses] = useState([]);
   const [fetchCounter, setFetchCounter] = useState(0);
-
-  const fetching = async (url, options) => {
-    // console.log(`url, options:`, url, options);
-    const fetchRes = await fetch(url, options)
-      .then((res) => res.json())
-      .then((body) => {
-        // console.log(`body:`, body);
-        return body;
-      })
-      .catch((error) => {
-        // console.log("Server failed to return data: " + error);
-        return error;
-      });
-    return fetchRes;
-  };
 
   const fetchDataFromAllCouriers = async (signal) => {
     // console.log(`state:`, state);
     setFetchCounter(0);
-    setAllResponses([{SLOW: []}, {MEDIUM: []}, {FAST: []}]);
+    setAllResponses([]);
     setData([]);
 
     state.forFetchingData.forEach(async (courierData) => {
-      let optionsData = {};
-      if (courierData.getToken) {
-        const optionsToken = {
-          ...courierData.getToken.options,
-          ...{url: courierData.getToken.url},
-        };
-        const tokken = await fetching(courierData.names.apiUrl, {
-          method: "POST",
-          body: JSON.stringify(optionsToken),
-          headers: {
-            "Content-Type": "application/json",
-          },
-          signal,
-        });
-        optionsData = {
-          ...courierData.getData.options,
-          headers: {
-            ...courierData.getData.options.headers,
-            Authorization: `Bearer ${tokken.access_token}`,
-          },
-          ...{url: courierData.getData.url},
-        };
-      }
-
-      optionsData = {
-        ...courierData.getData.options,
-        ...{url: courierData.getData.url},
-        ...optionsData,
-      };
-
-      const data = await fetching(courierData.names.apiUrl, {
-        method: "POST",
-        body: JSON.stringify(optionsData),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        signal,
-      });
+      const data = await getData(courierData, signal);
       // console.log(`data:`, data);
       const companyName = courierData.names.companyName;
       const formatedData = formattingData(companyName, data);
       // console.log(`formatedData:`, formatedData);
 
-      const allServices = [];
-      const allSpeed = [];
-      formatedData.forEach((e) => {
-        if (!allServices.includes(e.serviceName)) {
-          allServices.push(e.serviceName);
-        }
-        if (!allSpeed.includes(e.deliveryTime)) {
-          allSpeed.push(e.deliveryTime);
-        }
-        // {service: e.serviceName}
-      });
-
-      const sortedData = sortingData(formatedData);
       setAllResponses((prev) => {
-        const out = [];
-        const jonedArr = [
-          {
-            SLOW: [
-              ...prev.find((e) => e.SLOW).SLOW,
-              ...sortedData.find((e) => e.SLOW).SLOW,
-            ],
-          },
-          {
-            MEDIUM: [
-              ...prev.find((e) => e.MEDIUM).MEDIUM,
-              ...sortedData.find((e) => e.MEDIUM).MEDIUM,
-            ],
-          },
-          {
-            FAST: [
-              ...prev.find((e) => e.FAST).FAST,
-              ...sortedData.find((e) => e.FAST).FAST,
-            ],
-          },
-        ];
-        // console.log(`jonedArr:`, jonedArr);
-        allSpeed.forEach((e, i) => {
-          if (e !== "") {
-            const qq = [];
-            jonedArr.forEach((rr) => {
-              if (Object.keys(rr)[0] === e) {
-                qq.push(Object.values(rr));
-              }
-            });
-            const fi = qq[0][0].filter((ee) => ee.deliveryTime === e);
-            const allSer = [];
-
-            fi.forEach((ee) => {
-              if (!allSer.includes(ee.serviceName) && ee.serviceName !== "")
-                allSer.push(ee.serviceName);
-            });
-            const q = allSer.map((ee) => {
-              const data = fi.filter((eee) => eee.serviceName === ee);
-              data.sort(dynamicSort(`price`));
-              const min = Math.min(...data.map((item) => item.price));
-              const max = Math.max(...data.map((item) => item.price));
-              return {
-                min,
-                max,
-                data,
-                service: ee,
-              };
-            });
-            // console.log(`q:`, q);
-            const minPrice = Math.min(...q.map((item) => item.min));
-            const maxPrice = Math.max(...q.map((item) => item.max));
-            q.sort(dynamicSort(`min`));
-            out.push({
-              [e]: q,
-              minPrice,
-              maxPrice,
-            });
-          }
-        });
-        // console.log(`out:`, out);
-        setData(out);
-        return jonedArr;
+        const tempAllRes = [...prev, ...formatedData];
+        const newData = setNewData(tempAllRes, formatedData);
+        // console.log(`newData:`, newData);
+        setData(newData);
+        return tempAllRes;
       });
       setFetchCounter((prev) => prev + 1);
     });
   };
-
-  const sortingData = (formatedData) => {
-    const out = [{SLOW: []}, {MEDIUM: []}, {FAST: []}];
-    formatedData.forEach((e) => {
-      if (e.deliveryTime === FAST) {
-        const arr = out.find((e) => e.FAST).FAST;
-        arr.push(e);
-      }
-      if (e.deliveryTime === MEDIUM) {
-        const arr = out.find((e) => e.MEDIUM).MEDIUM;
-        arr.push(e);
-      }
-      if (e.deliveryTime === SLOW) {
-        const arr = out.find((e) => e.SLOW).SLOW;
-        arr.push(e);
-      }
-    });
-    return out;
-  };
-  const formattingData = (companyName, data) => {
-    const formatedData = [];
-    switch (companyName) {
-      case PARCEL2GO:
-        data.Quotes.forEach((item) => {
-          const courierName = normalizerNames.courierName(
-            item.Service.CourierName,
-            companyName
-          );
-          const serviceName = normalizerNames.serviceName(item.Service.Name, companyName);
-          const deliveryTime = normalizerNames.deliveryTime(
-            item.Service.Classification,
-            companyName
-          );
-          const price = item.TotalPrice.toFixed(2);
-
-          formatedData.push({
-            companyName,
-            courierName,
-            serviceName,
-            price,
-            deliveryTime,
-          });
-        });
-        break;
-
-      case PARCEL_MONKEY:
-        data.forEach((item) => {
-          const courierName = normalizerNames.courierName(item.carrier, companyName);
-          const serviceName = normalizerNames.serviceName(item.service, companyName);
-          const deliveryTime = normalizerNames.deliveryTime(
-            item.service_name,
-            companyName
-          );
-          const price = item.total_price_gross;
-          formatedData.push({
-            companyName,
-            courierName,
-            serviceName,
-            price,
-            deliveryTime,
-          });
-        });
-        break;
-
-      default:
-        break;
-    }
-    return formatedData;
-  };
-
-  // useEffect(() => {
-  //   const controller = new AbortController();
-  //   const signal = controller.signal;
-  //   fetchDataFromAllCouriers(signal);
-  //   return () => {
-  //     controller.abort();
-  //   };
-  // }, []);
 
   useEffect(() => {
     if (allResponses.length > 0) {
@@ -257,7 +53,6 @@ const Table = () => {
     if (tempController) tempController.abort();
 
     setTempController(controller);
-
     fetchDataFromAllCouriers(signal);
     console.log("fired");
     return () => {
@@ -268,8 +63,8 @@ const Table = () => {
 
   const sortByPrice = () => {
     const tempData = [...data];
-    tempData.forEach((e) => {
-      Object.values(e)[0].sort(dynamicSort(`${isPriceAsc ? "min" : "-max"}`));
+    tempData.forEach((timeSpeed) => {
+      timeSpeed.timeSpeedData.sort(dynamicSort(`${isPriceAsc ? "min" : "-max"}`));
     });
     setIsPriceAsc((prev) => !prev);
     setData(tempData);
@@ -281,28 +76,27 @@ const Table = () => {
       <button onClick={sortByPrice}>sortByPrice</button>
       <span>fetchCounter:{fetchCounter}</span>
       <div style={{display: "flex", columnGap: "20px"}}>
-        {data.map((res, i) => {
-          // console.log(`res:`, Object.values(res)[0][0]?.deliveryTime);
-          // console.log(`res:`, res);
+        {data.map((timeSpeed) => {
           return (
             <div
-              key={i}
-              className={Object.keys(res)[0]}
+              key={timeSpeed.id}
+              className={timeSpeed.deliveryTime}
               style={{display: "flex", flexDirection: "column"}}>
-              {Object.keys(res)[0]}
-              {Object.values(res)[0].map((e, ii) => {
-                // console.log(`e:`, e);
+              {timeSpeed.deliveryTime}
+              {timeSpeed.timeSpeedData.map((service) => {
                 return (
-                  <div key={ii} className={ii}>
+                  <div key={service.id} className={service.serviceName}>
                     <div>
                       <span>service:</span>
-                      <span style={{background: "lightgreen"}}>{e.service}</span>
+                      <span style={{background: "lightgreen"}}>
+                        {service.serviceName}
+                      </span>
                     </div>
-                    {e.data.map((ee, iii) => {
+                    {service.serviceData.map((company) => {
                       return (
-                        <div key={iii} style={{background: "lightblue"}}>
-                          <span>{ee.companyName}</span>
-                          <span>price: {ee.price}</span>
+                        <div key={company.id} style={{background: "lightblue"}}>
+                          <span>{company.companyName}</span>
+                          <span>price: {company.price}</span>
                         </div>
                       );
                     })}
---
+++ src/components/functions.js
@@ -0,0 +1,174 @@
+import normalizerNames from "../store/normalizerNames";
+import {ACTION_TYPES, COURIER_NAMES, VARIABLES} from "../store/postActionTypes";
+import {dynamicSort} from "../store/functions";
+
+const {PARCEL_MONKEY, PARCEL2GO} = COURIER_NAMES;
+
+export const fetching = async (url, options) => {
+  // console.log(`url, options:`, url, options);
+  const fetchRes = await fetch(url, options)
+    .then((res) => res.json())
+    .then((body) => {
+      // console.log(`body:`, body);
+      return body;
+    })
+    .catch((error) => {
+      // console.log("Server failed to return data: " + error);
+      return error;
+    });
+  return fetchRes;
+};
+
+export const getData = async (courierData, signal) => {
+  let optionsData = {};
+  if (courierData.getToken) {
+    const optionsToken = {
+      ...courierData.getToken.options,
+      ...{url: courierData.getToken.url},
+    };
+    const tokken = await fetching(courierData.names.apiUrl, {
+      method: "POST",
+      body: JSON.stringify(optionsToken),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      signal,
+    });
+    optionsData = {
+      ...courierData.getData.options,
+      headers: {
+        ...courierData.getData.options.headers,
+        Authorization: `Bearer ${tokken.access_token}`,
+      },
+      ...{url: courierData.getData.url},
+    };
+  }
+
+  optionsData = {
+    ...courierData.getData.options,
+    ...{url: courierData.getData.url},
+    ...optionsData,
+  };
+
+  const data = await fetching(courierData.names.apiUrl, {
+    method: "POST",
+    body: JSON.stringify(optionsData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    signal,
+  });
+  return data;
+};
+
+export const formattingData = (companyName, data) => {
+  const formatedData = [];
+  switch (companyName) {
+    case PARCEL2GO:
+      data.Quotes.forEach((item) => {
+        const courierName = normalizerNames.courierName(
+          item.Service.CourierName,
+          companyName
+        );
+        const serviceName = normalizerNames.serviceName(item.Service.Name, companyName);
+        const deliveryTime = normalizerNames.deliveryTime(
+          item.Service.Classification,
+          companyName
+        );
+        const price = item.TotalPrice.toFixed(2);
+
+        formatedData.push({
+          companyName,
+          courierName,
+          serviceName,
+          price,
+          deliveryTime,
+        });
+      });
+      break;
+
+    case PARCEL_MONKEY:
+      data.forEach((item) => {
+        const courierName = normalizerNames.courierName(item.carrier, companyName);
+        const serviceName = normalizerNames.serviceName(item.service, companyName);
+        const deliveryTime = normalizerNames.deliveryTime(item.service_name, companyName);
+        const price = item.total_price_gross;
+        formatedData.push({
+          companyName,
+          courierName,
+          serviceName,
+          price,
+          deliveryTime,
+        });
+      });
+      break;
+
+    default:
+      break;
+  }
+  return formatedData;
+};
+
+export const setNewData = (tempAllRes, formatedData) => {
+  //ie. onlyUniqueServicesName = ['DX 24H', 'DHL UK NextDay by 9am'] so 'DX 24H' will not repeat in this array
+  const onlyUniqueServicesName = [];
+
+  //like allDelveryTimes = [FAST', 'MEDIUM']
+  const allDelveryTimes = [];
+  formatedData.forEach((item) => {
+    if (!onlyUniqueServicesName.includes(item.serviceName)) {
+      onlyUniqueServicesName.push(item.serviceName);
+    }
+    if (!allDelveryTimes.includes(item.deliveryTime) && item.deliveryTime !== "") {
+      allDelveryTimes.push(item.deliveryTime);
+    }
+    // {service: e.serviceName}
+  });
+  // console.log(`allDelveryTimes:`, allDelveryTimes);
+
+  // console.log(`onlyUniqueServicesName:`, onlyUniqueServicesName);
+  const newData = allDelveryTimes.map((deliveryTime, idDelTime) => {
+    const filteredWithDelTime = tempAllRes.filter(
+      (singleData) => singleData.deliveryTime === deliveryTime
+    );
+    const allServicesNames = [];
+
+    filteredWithDelTime.forEach((singleData) => {
+      if (
+        !allServicesNames.includes(singleData.serviceName) &&
+        singleData.serviceName !== ""
+      )
+        allServicesNames.push(singleData.serviceName);
+    });
+
+    const tempData = allServicesNames.map((serviceName, idService) => {
+      const data = filteredWithDelTime.filter((item) => item.serviceName === serviceName);
+      const dataWithId = data.map((item, id) => {
+        return {...item, id};
+      });
+      dataWithId.sort(dynamicSort("price"));
+      const min = Math.min(...dataWithId.map((item) => item.price));
+      const max = Math.max(...dataWithId.map((item) => item.price));
+      return {
+        id: idService,
+        isAscending: true,
+        min,
+        max,
+        serviceData: dataWithId,
+        serviceName,
+      };
+    });
+    const minPrice = Math.min(...tempData.map((item) => item.min));
+    const maxPrice = Math.max(...tempData.map((item) => item.max));
+    tempData.sort(dynamicSort("min"));
+    return {
+      id: idDelTime,
+      deliveryTime,
+      timeSpeedData: tempData,
+      isAscending: true,
+      minPrice,
+      maxPrice,
+    };
+  });
+  return newData;
+};
